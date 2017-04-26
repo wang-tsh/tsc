7c7848a2efb943874e4282a2cc81d3c55105e9e1
wangtingshun <tingshun1989@163.com>
2017-04-26 17:19:07 +0800

    static web no vue

---
+++ webpack_config/base/module-base.config.js
@@ -0,0 +1,69 @@
+const dirVars = require('./dir-structure.config.js');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+  rules: [
+      {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          options: {
+              presets: [['es2015', { loose: true }]],
+              cacheDirectory: true,
+              plugins: ['transform-runtime'],
+          },
+      },
+      {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+      },
+      {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: "css-loader",
+              publicPath :"../"//坑死我了，之前完全不知道有这个参数，出来的图片位置都不对
+          })
+
+      },
+      {
+          test: /\.json$/,
+          loader: "json-loader" //这里在webpack 2中，必须加上-loader后缀
+      },
+      {
+          test: /\.html$/,
+          loader: "html-withimg-loader"
+      },
+    //
+    //
+    {
+      // 图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
+      // 如下配置，将小于8192byte的图片转成base64码
+      test: /\.(png|jpg|gif)$/,
+      include: dirVars.srcRootDir,
+      // loader: 'url-loader?limit=8192&name=./static/img/[hash].[ext]',
+      loader: 'url-loader',
+      options: {
+        limit: 8192,
+        name: './static/img/[hash].[ext]',
+      },
+    },
+    // {
+    //   // 专供bootstrap方案使用的，忽略bootstrap自带的字体文件
+    //   test: /\.(woff|woff2|svg|eot|ttf)$/,
+    //   include: /glyphicons/,
+    //   loader: 'null-loader',
+    // },
+    // {
+    //   // 专供iconfont方案使用的，后面会带一串时间戳，需要特别匹配到
+    //   test: /\.(woff|woff2|svg|eot|ttf)\??.*$/,
+    //   include: dirVars.srcRootDir,
+    //   // exclude: /glyphicons/,
+    //   // loader: 'file-loader?name=static/fonts/[name].[ext]',
+    //   loader: 'file-loader',
+    //   options: {
+    //     name: 'static/fonts/[name].[ext]',
+    //   },
+    // },
+
+  ],
+};
---
+++ webpack_config/module.config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wangtingshun on 2017/4/25.
+ */
+const path = require('path');
+const dirVars = require('./base/dir-structure.config.js');
+const  moduleConfig  = require('./base/module-base.config.js');
+module.exports = moduleConfig;
