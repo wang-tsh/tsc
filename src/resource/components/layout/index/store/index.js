72c9a0ca84f2acf4c5c381ef08e80a7b6bb4b042
wangtingshun <tingshun1989@163.com>
2017-05-15 09:50:49 +0800

    vue vuex init

--- src/resource/components/layout/index/store/index.js
+++ src/resource/components/layout/index/store/index.js
@@ -3,8 +3,8 @@
  */
 import Vue from 'vue'
 import Vuex from 'vuex'
-import topFrameStore from './modules/topFrameStore.js'
-import mainFrameStore from './modules/mainFrameStore.js'
+// import topFrameStore from './modules/topFrameStore.js'
+// import mainFrameStore from './modules/mainFrameStore.js'
 Vue.use(Vuex);
 export default new Vuex.Store({
     state:{
